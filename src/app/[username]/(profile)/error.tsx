77fee8709a8dabc3f94f93091aa90d3eb4cd0ea0
kingrayhan <rayhan095@gmail.com>
2024-05-21 00:50:45 +0600

    wip profile page

---
+++ src/app/[username]/(profile)/error.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import BaseLayout from "@/components/layout/BaseLayout";
+import React from "react";
+
+const ProfileError = () => {
+  return <BaseLayout>ProfileError</BaseLayout>;
+};
+
+export default ProfileError;
---
+++ src/app/[username]/(profile)/loading.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProfileLoading = () => {
+  return <div>ProfileLoading</div>;
+};
+
+export default ProfileLoading;
