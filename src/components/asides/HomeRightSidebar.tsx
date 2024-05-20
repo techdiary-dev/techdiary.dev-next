7fc54d901d6917dd3d1bc3682bef7c3c0cae33ef
kingrayhan <rayhan095@gmail.com>
2024-05-20 09:36:44 +0600

    social login widget

--- src/components/asides/HomeRightSidebar.tsx
+++ src/components/asides/HomeRightSidebar.tsx
@@ -3,10 +3,12 @@ import SocialLinksWidget from "./widgets/SocialLinksWidget";
 import ImportantLinksWidget from "./widgets/ImportantLinksWidget";
 import DiscordWidget from "./widgets/DiscordWidget";
 import LatestUsers from "./widgets/LatestUsers";
+import SocialLogin from "./widgets/SocialLogin";
 
 const HomeRightSidebar = () => {
   return (
     <div className="flex flex-col gap-6 p-2">
+      <SocialLogin />
       <SocialLinksWidget />
       <ImportantLinksWidget />
       <DiscordWidget />
