be5f82aa1a8dda88e20e389615fdce72a8bfb191
kingrayhan <rayhan095@gmail.com>
2024-06-02 11:55:30 +0600

    dashboard hamburger menu for mobile

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,12 +2,18 @@ import Link from "next/link";
 import DarkLogo from "../logos/DarkLogo";
 import NavbarAction from "./NavbarAction";
 import SearchInput from "./SearchInput";
+import React from "react";
 
-const Navbar = () => {
+interface NavbarProps {
+  Trailing?: React.ReactNode;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ Trailing }) => {
   return (
     <div className="navbar app-border-color">
       <div className="navbar__inner wrapper">
         <div className="navbar__left">
+          {Trailing}
           <Link href="/">
             <DarkLogo className={"navbar__logo"} />
           </Link>
