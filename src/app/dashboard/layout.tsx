54f9ecb59c746b885a6c807f3ac4801a44b89335
kingrayhan <rayhan095@gmail.com>
2024-06-02 13:25:36 +0600

    dashboard loading page

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,5 @@
 import React, { PropsWithChildren } from "react";
 import DashboardLayout from "./_components/DashboardLayout";
-import { Metadata } from "next";
 
 const layout: React.FC<PropsWithChildren> = ({ children }) => {
   return <DashboardLayout>{children}</DashboardLayout>;
