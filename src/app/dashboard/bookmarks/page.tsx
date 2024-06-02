352e463cc9cf89b8dcc322b13e24c8653dd73e17
kingrayhan <rayhan095@gmail.com>
2024-06-02 13:14:59 +0600

    wip: dashboard/articles

--- src/app/dashboard/bookmarks/page.tsx
+++ src/app/dashboard/bookmarks/page.tsx
@@ -1,10 +1,6 @@
 import { Metadata } from "next";
 import React from "react";
 
-export const meta: Metadata = {
-  title: "Bookmarks",
-};
-
 const BookmarkPage = () => {
   return <div>BookmarkPage</div>;
 };
