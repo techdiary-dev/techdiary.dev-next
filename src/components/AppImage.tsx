50eb75907a9b82c964e6b9296e5c15a626152a69
kingrayhan <rayhan095@gmail.com>
2024-06-03 10:36:51 +0600

    dashboard states

--- src/components/AppImage.tsx
+++ src/components/AppImage.tsx
@@ -2,6 +2,7 @@ import { IAppImage } from "@/http/models/AppImage.model";
 import { AdvancedImage } from "@cloudinary/react";
 import Image from "next/image";
 import { blur } from "@cloudinary/url-gen/actions/effect";
+import { auto } from "@cloudinary/url-gen/actions/resize";
 
 import { Cloudinary } from "@cloudinary/url-gen";
 import React from "react";
@@ -21,46 +22,41 @@ const AppImage: React.FC<AppImageProps> = ({
   height,
   width,
 }) => {
-  switch (imageSource?.provider) {
-    case "cloudinary":
-      const cld = new Cloudinary({
-        cloud: { cloudName: "techdiary-dev" },
-      });
-      const _image = cld.image(imageSource?.key).quality("auto").format("auto");
+  const cld = new Cloudinary({
+    cloud: { cloudName: "techdiary-dev" },
+  });
+  let _imageUrl = "";
+  let _blurredImageUrl = "/thumbnail-placeholder.png";
 
-      return (
-        <Image
-          alt={alt || ""}
-          src={_image.toURL()}
-          sizes={sizes}
-          height={height}
-          width={width}
-          placeholder="blur"
-          blurDataURL="/thumbnail-placeholder.png"
-        />
-      );
-    // return JSON.stringify(_image.toURL());
-    case "direct":
-      return (
-        <Image
-          src={imageSource?.key}
-          alt={alt || ""}
-          sizes={sizes}
-          width={width}
-          height={height}
-        />
-      );
-    default:
-      return (
-        <Image
-          src={imageSource?.key || ""}
-          alt={alt || ""}
-          sizes={sizes}
-          width={width}
-          height={height}
-        />
-      );
+  if (imageSource?.provider === "cloudinary") {
+    _imageUrl = cld
+      .image(imageSource?.key)
+      .quality("auto")
+      .format("auto")
+      .toURL();
+    _blurredImageUrl = cld
+      .image(imageSource?.key)
+      .quality("auto")
+      .format("auto")
+      .effect(blur(100000))
+      .toURL();
+  } else {
+    _imageUrl = imageSource?.key || "";
   }
+
+  return (
+    <Image
+      alt={alt || ""}
+      src={_imageUrl}
+      sizes={sizes}
+      height={height}
+      width={width}
+      placeholder="blur"
+      loading="lazy"
+      className="w-full"
+      blurDataURL={_blurredImageUrl}
+    />
+  );
 };
 
 export default AppImage;
