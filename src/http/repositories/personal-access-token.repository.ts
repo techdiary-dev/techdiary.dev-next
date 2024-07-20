be742f803dd03cca4a4cf2fabcc2881135fa7600
kingrayhan <rayhan095@gmail.com>
2024-07-20 17:20:06 +0600

    personal access token: delete button

--- src/http/repositories/personal-access-token.repository.ts
+++ src/http/repositories/personal-access-token.repository.ts
@@ -9,10 +9,26 @@ export class PersonalAccessTokenApiRepository extends ApiRepository {
 
     return data;
   }
+
+  public async createToken(payload: { name: string }) {
+    const { data } = await this.http.post<{ token: string }>(
+      "/api/auth/personal-access-tokens",
+      payload
+    );
+
+    return data;
+  }
 }
 
 // curl \
 //   -X GET \
 //   -H "Accept: application/json" \
 //   -H "Authorization: Bearer 9|BQm6KnOTnY3i4IlBF4eZhgrvzLudAuNLr6xvvEeP3b1ad00e" \
+//   http://api.techdiary.test/api/auth/personal-access-tokens
+
+// curl \
+//   -X POST -# \
+//   -H "Accept: application/json" \
+//   -H "Authorization: Bearer 9|BQm6KnOTnY3i4IlBF4eZhgrvzLudAuNLr6xvvEeP3b1ad00e" \
+//   -d "name=Test token" \
 //   http://api.techdiary.test/api/auth/personal-access-tokens
