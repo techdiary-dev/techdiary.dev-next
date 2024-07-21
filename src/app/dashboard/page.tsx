fc1dba1ccb772a23f63d43cb88dcf5c2d92c91d2
kingrayhan <rayhan095@gmail.com>
2024-07-21 11:50:37 +0600

    dashboard empty illustration

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,15 @@
 import { Metadata } from "next";
-import React from "react";
+import React, { Suspense } from "react";
 import DashboardPage from "./page.client";
 import { ArticleApiRepository } from "@/http/repositories/article.repository";
 import { cookieHeaders } from "@/utils/ssr-user";
 import { Paper, Text } from "@mantine/core";
 import _t from "@/i18n/_t";
+import Image from "next/image";
+import Link from "next/link";
 
 export const dynamic = "force-dynamic";
-export const revalidate = 0;
+export const revalidate = 1;
 
 export const metadata: Metadata = {
   title: "Dashboard",
@@ -38,7 +40,47 @@ const Dashboard = async () => {
           <Text size={"35px"}>{articles?.meta?.counts.comments || 0}</Text>
         </Paper>
       </div>
-      <DashboardPage initialArticles={articles} />
+
+      {!Boolean(articles?.meta?.total) && (
+        <div className="p-4 mt-6 border-2 border-dashed app-border-color">
+          <Image
+            className="mx-auto lg:w-4/12"
+            src="/images/fabulous-late-night-working.png"
+            width={1200}
+            height={1200}
+            alt={""}
+          />
+
+          <h3 className="my-6 text-xl font-semibold text-center text-dark-secondary">
+            {_t("You didn't write any article yet")}
+          </h3>
+
+          <div className="flex justify-center space-x-4">
+            <Link
+              href={"/dashboard/articles/new"}
+              className="flex items-center px-2 py-1 space-x-1 text-white rounded bg-slate-800"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-5 h-5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+              <span>{_t("New diary")}</span>
+            </Link>
+          </div>
+        </div>
+      )}
+
+      <Suspense fallback={<h1>Loading....</h1>}>
+        <DashboardPage initialArticles={articles} />
+      </Suspense>
     </div>
   );
 };
