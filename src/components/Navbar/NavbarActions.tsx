8fdcb7b08aaad02d15d5cd82135d84f47fd7040f
kingrayhan <rayhan095@gmail.com>
2025-03-20 16:49:05 +0600

    Refactor Navbar and ThemeSwitcher components for improved UI and functionality
    - Updated Navbar to include logo, search input, and action buttons for enhanced navigation.
    - Introduced a Trailing prop for additional content in the Navbar.
    - Enhanced ThemeSwitcher with SVG icons for light and dark mode toggling.
    - Removed unused CSS files related to Navbar and search input for better maintainability.
    - Updated app.css with new CSS variable definitions for improved theme support.

---
+++ src/components/Navbar/NavbarActions.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "../ui/button";
+
+const NavbarActions = () => {
+  return (
+    <div>
+      <Button>নতুন ডায়েরি</Button>
+    </div>
+  );
+};
+
+export default NavbarActions;
