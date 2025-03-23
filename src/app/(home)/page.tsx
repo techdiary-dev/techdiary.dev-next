a655ba20e297af5703a5666a67dd9024719db5f2
kingrayhan <rayhan095@gmail.com>
2025-03-23 17:50:50 +0600

    Remove session retrieval and debug output from home page component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,14 +4,12 @@ import { Metadata } from "next";
 import HomeLeftSidebar from "./_components/HomeLeftSidebar";
 import HomeRightSidebar from "./_components/HomeRightSidebar";
 import SidebarToggleButton from "./_components/SidebarToggleButton";
-import { getSession } from "@/auth/auth";
 
 export const metadata: Metadata = {
   title: "Home",
 };
 
 const Page = async () => {
-  const session = await getSession();
   return (
     <HomepageLayout
       LeftSidebar={<HomeLeftSidebar />}
@@ -19,7 +17,6 @@ const Page = async () => {
       NavbarTrailing={<SidebarToggleButton />}
     >
       <h1>{_t("Home")}</h1>
-      <pre>{JSON.stringify(session, null, 2)}</pre>
     </HomepageLayout>
   );
 };
