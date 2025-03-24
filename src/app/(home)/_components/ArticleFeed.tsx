c6ae7fb5db1053ba14f3e1b819dd376a52010f35
kingrayhan <rayhan095@gmail.com>
2025-03-24 16:16:09 +0600

    Enhance ArticleCard and ArticleFeed components with article handle and username
    - Added 'handle' and 'username' properties to ArticleCard for improved article linking.
    - Updated ArticleFeed to pass new properties to ArticleCard.
    - Implemented dynamic article URLs in ArticleCard for better navigation.

--- src/app/(home)/_components/ArticleFeed.tsx
+++ src/app/(home)/_components/ArticleFeed.tsx
@@ -28,12 +28,14 @@ const ArticleFeed = () => {
         <ArticleCard
           key={article.id}
           id={article.id}
+          handle={article.handle}
           title={article.title}
           excerpt={article.excerpt ?? ""}
           coverImage={getFileUrl(article.cover_image!)}
           author={{
             name: article.user?.name ?? "",
             avatar: article.user?.profile_photo ?? "",
+            username: article.user?.username ?? "",
           }}
           publishedAt={article.created_at.toDateString()}
           readingTime={readingTime(article.body ?? "")}
