e9f6ee73efa5146cdb5088ddf8d45190a40fc906
kingrayhan <rayhan095@gmail.com>
2025-03-25 07:01:09 +0600

    Enhance article display in page.tsx and update styles
    - Integrated markdownToHtml utility to parse article body content for rendering.
    - Added AppImage component for displaying article cover images.
    - Updated content-typography styles for improved layout and responsiveness.
    - Imported content-typography styles into app.css for consistent styling across the application.

--- src/app/[username]/[articleHandle]/page.tsx
+++ src/app/[username]/[articleHandle]/page.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import { NextPage } from "next";
 import HomepageLayout from "@/components/layout/HomepageLayout";
 import HomeLeftSidebar from "@/app/(home)/_components/HomeLeftSidebar";
+import { markdownToHtml } from "@/utils/markdoc-parser";
+import AppImage from "@/components/AppImage";
 
 interface ArticlePageProps {
   params: Promise<{
@@ -16,11 +18,32 @@ const Page: NextPage<ArticlePageProps> = async ({ params }) => {
   const _params = await params;
   const article = await articleActions.articleDetail(_params.articleHandle);
 
+  const parsedHTML = markdownToHtml(article?.body ?? "");
+
   return (
     <HomepageLayout LeftSidebar={<HomeLeftSidebar />}>
-      {!article && <div>Article not found</div>}
+      {/* {!article && <div>Article not found</div>} */}
+      <div className="px-4 my-2 md:m-0">
+        {article?.cover_image && (
+          <div className="rounded-sm w-full overflow-hidden">
+            <AppImage
+              alt={article?.title ?? ""}
+              imageSource={article?.cover_image}
+              width={1200}
+              height={630}
+            />
+          </div>
+        )}
 
-      <pre>{JSON.stringify({ params, article }, null, 2)}</pre>
+        <div className="my-6">
+          <h1 className="text-3xl font-bold">{article?.title ?? ""}</h1>
+        </div>
+
+        <div
+          className="mx-auto content-typography"
+          dangerouslySetInnerHTML={{ __html: parsedHTML }}
+        ></div>
+      </div>
     </HomepageLayout>
   );
 };
