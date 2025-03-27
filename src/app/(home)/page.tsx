e4786f87b5bdac753e31e5383c0783861b48bd33
kingrayhan <rayhan095@gmail.com>
2025-03-28 02:21:57 +0600

    Refactor home page component and clean up social links widget
    - Changed the home page component from an async function to a regular function for improved performance.
    - Removed commented-out session retrieval code from the home page.
    - Eliminated unnecessary title tags from the SocialLinksWidget for cleaner code.
    - Added missing link for the Instagram social media icon in the SocialLinksWidget.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,20 +1,17 @@
 import HomepageLayout from "@/components/layout/HomepageLayout";
-import { Metadata } from "next";
 import ArticleFeed from "./_components/ArticleFeed";
 import FakeEditor from "./_components/FakeEditor";
 import HomeLeftSidebar from "./_components/HomeLeftSidebar";
 import HomeRightSidebar from "./_components/HomeRightSidebar";
 import SidebarToggleButton from "./_components/SidebarToggleButton";
 
-const Page = async () => {
-  // const session = await getSession();
+const Page = () => {
   return (
     <HomepageLayout
       LeftSidebar={<HomeLeftSidebar />}
       RightSidebar={<HomeRightSidebar />}
       NavbarTrailing={<SidebarToggleButton />}
     >
-      {/* <pre>{JSON.stringify(session, null, 2)}</pre> */}
       <div className="px-4 my-2 md:m-0">
         <FakeEditor />
       </div>
