bde0b73fb42b1896f800f07c50944de49444e0c6
kingrayhan <rayhan095@gmail.com>
2025-03-29 23:44:34 +0600

    Update dashboard pages to include feature notice and icon
    - Replaced placeholder text with a notice indicating that the feature is not yet complete.
    - Added ExclamationTriangleIcon for visual emphasis on the notice.
    - Updated BookmarksPage, NotificationPage, SessionsPage, and SettingsPage components for consistency.

--- src/app/dashboard/bookmarks/page.tsx
+++ src/app/dashboard/bookmarks/page.tsx
@@ -1,7 +1,13 @@
+import { ExclamationTriangleIcon } from "@radix-ui/react-icons";
 import React from "react";
 
 const BookmarksPage = () => {
-  return <div>BookmarksPage</div>;
+  return (
+    <div className="flex flex-col gap-2">
+      <ExclamationTriangleIcon className="size-10 text-yellow-400" />
+      <h2 className=" text-2xl">এই ফিচার এখনো শেষ হয়নি</h2>
+    </div>
+  );
 };
 
 export default BookmarksPage;
