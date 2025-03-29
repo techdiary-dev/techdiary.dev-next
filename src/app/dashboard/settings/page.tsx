eb3e5ee923b71127ded7c977f25dfd2baab42025
kingrayhan <rayhan095@gmail.com>
2025-03-30 00:43:41 +0600

    Update SettingsPage to implement tabbed navigation and add new Radix UI Tabs component
    - Replaced existing content with a tabbed interface for better organization of settings.
    - Integrated Radix UI Tabs for improved user experience and navigation.
    - Added translations for tab labels using the i18n module.

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -1,11 +1,23 @@
-import { ExclamationTriangleIcon } from "@radix-ui/react-icons";
-import React from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import _t from "@/i18n/_t";
 
 const SettingsPage = () => {
   return (
-    <div className="flex flex-col gap-2">
-      <ExclamationTriangleIcon className="size-10 text-yellow-400" />
-      <h2 className=" text-2xl">এই ফিচার এখনো শেষ হয়নি</h2>
+    <div>
+      <Tabs defaultValue="account">
+        <TabsList>
+          <TabsTrigger value="general">{_t("General")}</TabsTrigger>
+          <TabsTrigger value="social">{_t("Social")}</TabsTrigger>
+          <TabsTrigger value="profile_readme">
+            {_t("Profile Readme")}
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="general">Change your password here.</TabsContent>
+        <TabsContent value="social">Change your password here.</TabsContent>
+        <TabsContent value="profile_readme">
+          Change your profile readme here.
+        </TabsContent>
+      </Tabs>
     </div>
   );
 };
