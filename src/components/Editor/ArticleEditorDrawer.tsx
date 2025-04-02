1c202b3733abd76c96cb81d985074b646be60fa0
kingrayhan <rayhan095@gmail.com>
2025-04-02 15:27:26 +0600

    Update dependencies and enhance ArticleEditorDrawer with new form components and input handling

--- src/components/Editor/ArticleEditorDrawer.tsx
+++ src/components/Editor/ArticleEditorDrawer.tsx
@@ -1,12 +1,29 @@
+"use client";
+
 import { Article } from "@/backend/models/domain-models";
 import * as articleActions from "@/backend/services/article.actions";
 import { ArticleRepositoryInput } from "@/backend/services/inputs/article.input";
+import { useDebouncedCallback } from "@/hooks/use-debounced-callback";
+import { useTranslation } from "@/i18n/use-translation";
+import { useSession } from "@/store/session.atom";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useMutation } from "@tanstack/react-query";
+import { LinkIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
 import React from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { z } from "zod";
+import { Button } from "../ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
 import {
   Sheet,
   SheetContent,
@@ -21,7 +38,13 @@ interface Props {
   onSave: () => void;
 }
 const ArticleEditorDrawer: React.FC<Props> = ({ article, open, onClose }) => {
+  const session = useSession();
+  const { _t } = useTranslation();
   const router = useRouter();
+  const setDebounceHandler = useDebouncedCallback(async (slug: string) => {
+    const handle = await articleActions.getUniqueArticleHandle(slug);
+    form.setValue("handle", handle);
+  }, 2000);
   const updateMyArticleMutation = useMutation({
     mutationFn: (
       input: z.infer<typeof ArticleRepositoryInput.updateMyArticleInput>
@@ -71,16 +94,47 @@ const ArticleEditorDrawer: React.FC<Props> = ({ article, open, onClose }) => {
       },
     });
   };
-
+  //className="m-3 h-[100vh-20px] w-[100vw-20px]"
   return (
     <Sheet open={open} onOpenChange={onClose}>
-      <SheetContent className="m-3 h-[100vh-20px] w-[100vw-20px]">
+      <SheetContent>
         <SheetHeader>
           <SheetDescription asChild>
-            <form onSubmit={form.handleSubmit(handleOnSubmit)}>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </form>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(handleOnSubmit)}
+                className="flex flex-col gap-2"
+              >
+                <FormField
+                  control={form.control}
+                  name="handle"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>{_t("Handle")}</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Your handle"
+                          Prefix={
+                            <LinkIcon className="size-3 text-muted-foreground" />
+                          }
+                          {...field}
+                          onChange={(e) => {
+                            setDebounceHandler(e.target.value);
+                            form.setValue("handle", e.target.value);
+                          }}
+                        />
+                      </FormControl>
+                      <FormDescription className="-mt-1">
+                        https://www.techdiary.dev/{session?.user?.username}/
+                        {form.watch("handle")}
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button>{_t("Save")}</Button>
+              </form>
+            </Form>
           </SheetDescription>
         </SheetHeader>
       </SheetContent>
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,21 +1,46 @@
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-function Input({ className, type, ...props }: React.ComponentProps<"input">) {
-  return (
-    <input
-      type={type}
-      data-slot="input"
-      className={cn(
-        "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
-        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
-        className
-      )}
-      {...props}
-    />
-  )
+interface InputProps extends React.ComponentProps<"input"> {
+  Prefix?: React.ReactNode;
+  Suffix?: React.ReactNode;
 }
 
-export { Input }
+function Input({ className, Prefix, Suffix, type, ...props }: InputProps) {
+  return (
+    <div className="relative w-full">
+      {Prefix && (
+        <span
+          data-slot="prefix"
+          className="absolute inset-y-0 pl-2 left-0 flex items-center pointer-events-none"
+        >
+          {Prefix}
+        </span>
+      )}
+      <input
+        type={type}
+        data-slot="input"
+        className={cn(
+          "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+          "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+          Prefix && "pl-6!",
+          Suffix && "pr-6!",
+          className
+        )}
+        {...props}
+      />
+      {Suffix && (
+        <span
+          data-slot="suffix"
+          className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none"
+        >
+          {Suffix}
+        </span>
+      )}
+    </div>
+  );
+}
+
+export { Input };
