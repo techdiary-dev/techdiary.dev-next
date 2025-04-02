3645243cbf8db25ef9711244265df11430d9d64b
kingrayhan <rayhan095@gmail.com>
2025-04-02 09:14:51 +0600

    Integrate react-hook-form and zod for form handling in ArticleEditorDrawer

--- src/components/Editor/ArticleEditorDrawer.tsx
+++ src/components/Editor/ArticleEditorDrawer.tsx
@@ -7,6 +7,10 @@ import {
   SheetHeader,
   SheetTitle,
 } from "../ui/sheet";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { ArticleRepositoryInput } from "@/backend/services/inputs/article.input";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 interface Props {
   article: Article;
@@ -15,6 +19,23 @@ interface Props {
   onSave: () => void;
 }
 const ArticleEditorDrawer: React.FC<Props> = ({ article, open, onClose }) => {
+  const form = useForm<
+    z.infer<typeof ArticleRepositoryInput.updateMyArticleInput>
+  >({
+    defaultValues: {
+      handle: article?.handle ?? "",
+      excerpt: article?.excerpt ?? "",
+      metadata: {
+        seo: {
+          title: article?.metadata?.seo?.title ?? "",
+          description: article?.metadata?.seo?.description ?? "",
+          keywords: article?.metadata?.seo?.keywords ?? [],
+          canonical_url: article?.metadata?.seo?.canonical_url ?? "",
+        },
+      },
+    },
+    resolver: zodResolver(ArticleRepositoryInput.updateMyArticleInput),
+  });
   return (
     <Sheet open={open} onOpenChange={onClose}>
       <SheetContent className="m-3 h-[100vh-20px] w-[100vw-20px]">
