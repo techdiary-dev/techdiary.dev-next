07ad9f8dc5648c1f9f9989272eea8bbb69197b6e
kingrayhan <rayhan095@gmail.com>
2025-04-05 15:48:32 +0600

    Remove TagInput component from Page and simplify the return structure for cleaner code.

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,15 +1,10 @@
 "use client";
 
-import { TagInput } from "@/components/ui/tags-input";
 import React from "react";
 
 const Page = () => {
   const [tags, setTags] = React.useState<string[]>([]);
-  return (
-    <div className="p-10">
-      <TagInput tags={tags} onTagsChange={setTags} />
-    </div>
-  );
+  return <div className="p-10"></div>;
 };
 
 export default Page;
