621efcbc333ca6df13eb267c0305c63dc446c756
kingrayhan <rayhan095@gmail.com>
2025-04-05 15:53:26 +0600

    Update URL construction in sitemap to include '@' prefix for usernames, enhancing link structure for articles.

--- src/app/sitemaps/articles/sitemap.tsx
+++ src/app/sitemaps/articles/sitemap.tsx
@@ -29,7 +29,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     .map((article) => {
       let url = "null";
       if (article?.handle) {
-        url = `https://www.techdiary.dev/${article.user?.username}/${article?.handle}`;
+        url = `https://www.techdiary.dev/@${article.user?.username}/${article?.handle}`;
       }
 
       return {
