4270aa6e326bca456d4697e97f42ce40da249436
kingrayhan <rayhan095@gmail.com>
2025-04-05 16:39:02 +0600

    Add schema.org structured data for articles and update package.json with schema-dts dependency

--- src/app/[username]/[articleHandle]/page.tsx
+++ src/app/[username]/[articleHandle]/page.tsx
@@ -1,14 +1,16 @@
 import HomeLeftSidebar from "@/app/(home)/_components/HomeLeftSidebar";
 import * as articleActions from "@/backend/services/article.actions";
 import AppImage from "@/components/AppImage";
+import type { Article, WithContext } from "schema-dts";
 import HomepageLayout from "@/components/layout/HomepageLayout";
 import { readingTime, removeMarkdownSyntax } from "@/lib/utils";
 import { markdownToHtml } from "@/utils/markdoc-parser";
 import { Metadata, NextPage } from "next";
+import Image from "next/image";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 import ArticleSidebar from "./_components/ArticleSidebar";
-import Image from "next/image";
+import getFileUrl from "@/utils/getFileUrl";
 
 export const metadata: Metadata = {
   title: "Article detail",
@@ -27,6 +29,20 @@ const Page: NextPage<ArticlePageProps> = async ({ params }) => {
     _params.articleHandle
   );
 
+  const jsonLd: WithContext<Article> = {
+    "@context": "https://schema.org",
+    "@type": "Article",
+    name: article?.title,
+    image: getFileUrl(article?.cover_image),
+    description: article?.excerpt ?? removeMarkdownSyntax(article?.body ?? ""),
+    author: {
+      "@type": "Person",
+      name: article?.user?.name,
+      image: article?.user?.profile_photo,
+      url: `https://www.techdiary.dev/@${article?.user?.username}`,
+    },
+  };
+
   if (!article) {
     throw notFound();
   }
@@ -34,69 +50,79 @@ const Page: NextPage<ArticlePageProps> = async ({ params }) => {
   const parsedHTML = markdownToHtml(article?.body ?? "");
 
   return (
-    <HomepageLayout
-      LeftSidebar={<HomeLeftSidebar />}
-      RightSidebar={<ArticleSidebar article={article} />}
-    >
-      {/* {!article && <div>Article not found</div>} */}
-      <div className="px-4 my-2 md:m-0">
-        {article?.cover_image && (
-          <div className="rounded-sm w-full overflow-hidden">
-            <AppImage
-              alt={article?.title ?? ""}
-              imageSource={article?.cover_image}
-              width={1200}
-              height={630}
-            />
-          </div>
-        )}
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
 
-        {/* User information */}
-        <div className="mb-4 flex items-center my-4">
-          <div className="relative rounded-full overflow-hidden border transition-transform duration-300 size-10">
-            <Image
-              src={article?.user?.profile_photo ?? ""}
-              alt={article?.user?.username ?? ""}
-              width={40}
-              height={40}
-              className="w-full h-full object-cover transition-opacity duration-300 ease-in-out opacity-100"
-            />
-          </div>
+      <HomepageLayout
+        LeftSidebar={<HomeLeftSidebar />}
+        RightSidebar={<ArticleSidebar article={article} />}
+      >
+        {/* {!article && <div>Article not found</div>} */}
+        <div className="px-4 my-2 md:m-0">
+          {article?.cover_image && (
+            <div className="rounded-sm w-full overflow-hidden">
+              <AppImage
+                alt={article?.title ?? ""}
+                imageSource={article?.cover_image}
+                width={1200}
+                height={630}
+              />
+            </div>
+          )}
 
-          <div className="ml-2.5">
-            <Link
-              href={`/${article?.user?.username}`}
-              className="text-md font-medium text-foreground"
-            >
-              {article?.user?.name}
-            </Link>
-            <div className="flex items-center text-xs text-muted-foreground">
-              <time dateTime={article?.published_at?.toString()}>
-                {new Date(article?.published_at!).toLocaleDateString("bn-BD", {
-                  month: "long",
-                  day: "numeric",
-                  year: "numeric",
-                })}
-              </time>
-              <span className="mx-1.5">·</span>
-              <span>
-                {readingTime(removeMarkdownSyntax(article?.body ?? "")!)} min
-                read
-              </span>
+          {/* User information */}
+          <div className="mb-4 flex items-center my-4">
+            <div className="relative rounded-full overflow-hidden border transition-transform duration-300 size-10">
+              <Image
+                src={article?.user?.profile_photo ?? ""}
+                alt={article?.user?.username ?? ""}
+                width={40}
+                height={40}
+                className="w-full h-full object-cover transition-opacity duration-300 ease-in-out opacity-100"
+              />
+            </div>
+
+            <div className="ml-2.5">
+              <Link
+                href={`/${article?.user?.username}`}
+                className="text-md font-medium text-foreground"
+              >
+                {article?.user?.name}
+              </Link>
+              <div className="flex items-center text-xs text-muted-foreground">
+                <time dateTime={article?.published_at?.toString()}>
+                  {new Date(article?.published_at!).toLocaleDateString(
+                    "bn-BD",
+                    {
+                      month: "long",
+                      day: "numeric",
+                      year: "numeric",
+                    }
+                  )}
+                </time>
+                <span className="mx-1.5">·</span>
+                <span>
+                  {readingTime(removeMarkdownSyntax(article?.body ?? "")!)} min
+                  read
+                </span>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="my-6">
-          <h1 className="text-2xl font-bold">{article?.title ?? ""}</h1>
-        </div>
+          <div className="my-6">
+            <h1 className="text-2xl font-bold">{article?.title ?? ""}</h1>
+          </div>
 
-        <div
-          className="mx-auto content-typography"
-          dangerouslySetInnerHTML={{ __html: parsedHTML }}
-        ></div>
-      </div>
-    </HomepageLayout>
+          <div
+            className="mx-auto content-typography"
+            dangerouslySetInnerHTML={{ __html: parsedHTML }}
+          ></div>
+        </div>
+      </HomepageLayout>
+    </>
   );
 };
 
